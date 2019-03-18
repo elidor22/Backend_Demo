28c37cd2f4df26807288410f48871773198aac46
elidor <elidorv@outlook.com>
2019-03-18 19:20:56 +0100

    First test GIT

---
+++ src/main/java/com/Backend_Aplication_Start/Backend_Demo/BackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Backend_Aplication_Start.Backend_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDemoApplication.class, args);
+	}
+
+}
